ce47d047357849d5a7f1abcbfdcf7803cd3e0cb9
CrioUser <crio-user@criodo.com>
2022-08-26 13:52:48 +0530

    “ME_QTRIPDYNAMIC_MODULE_CITIES”

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "http://65.0.40.89:8082" };
+const config = { backendEndpoint: "http://13.232.223.14:8082" };
 
 export default config;
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -1,9 +1,10 @@
 import config from "../conf/index.js";
-
 async function init() {
   //Fetches list of all cities along with their images and description
+  console.log("init()")
+  console.log(config);
   let cities = await fetchCities();
-
+  console.log(cities)
   //Updates the DOM with the cities
   cities.forEach((key) => {
     addCityToDOM(key.id, key.city, key.description, key.image);
@@ -14,6 +15,13 @@ async function init() {
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
+  try{
+    const res=await fetch(config.backendEndpoint+'/cities');
+    const data=await res.json();
+    return data;
+  }catch(err){
+    return null;
+  }
 
 }
 
@@ -21,6 +29,20 @@ async function fetchCities() {
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  let ele=document.createElement("div");
+  ele.setAttribute("class","col-12 col-sm-6 col-lg-3 mb-4")
+  ele.innerHTML+=`
+  <a href="pages/adventures/?city=${id}"id="${id}">
+    <div class="tile">
+      <div class="tile-text text-center">
+        <h4>${city}</h4>
+        <h5>${description}</h5>
+      </div>
+      <img src="${image}" />
+    </div>
+  </a>`;
+  document.getElementById("data").appendChild(ele);
+
 
 }
 
