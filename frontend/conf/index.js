1ce831e7f3417060606309d756e0dbe2d79c55ef
vernal-spark <92166835+vernal-spark@users.noreply.github.com>
2023-03-08 19:54:42 +0530

    Update index.js

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,4 @@
 
-const config = { backendEndpoint: "https://harshith-qtrip-dynamic.herokuapp.com" };
+const config = { backendEndpoint: "https://qtrip-dynamic-oiu6.onrender.com" };
 
 export default config;
